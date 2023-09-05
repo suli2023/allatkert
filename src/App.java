90bc021e632af4c4ad49c82b1a49eebcce9d6fb2
Nagy János <janos@zold.lan>
2023-09-05 12:07:26 +0200

    A Kert osztály hozzáadva

--- src/App.java
+++ src/App.java
@@ -1,16 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Maci maci = new Maci();
-        Macska mici = new Macska();
-        maci.hangotad("Brumm...");
-        maci.mezgyujtes();
-        //maci.nev = "Brumi";
-
-        mici.hangotad("miauuu...");
-        mici.dorombol();
-        //mici.nev = "";
-        mici.beallitNev("");
-        //System.out.println(mici.nev);
-
+        Kert kert = new Kert();
+        kert.inditElet();
     }
 }
---
+++ src/Kert.java
@@ -0,0 +1,15 @@
+public class Kert {
+    public void inditElet() {
+        Maci maci = new Maci();
+        Macska mici = new Macska();
+        maci.hangotad("Brumm...");
+        maci.mezgyujtes();
+        //maci.nev = "Brumi";
+
+        mici.hangotad("miauuu...");
+        mici.dorombol();
+        //mici.nev = "";
+        mici.beallitNev("");
+        //System.out.println(mici.nev);
+    }
+}
